9274298f48ef337c36f8d25f3b8636bdf0835d54
Suzy Mueller <suzmue@golang.org>
2024-02-21 16:32:00 -0800

    add go env for debugging

--- idx-template.nix
+++ idx-template.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   packages = [ pkgs.go ];
   bootstrap = ''
+    go env
     go install golang.org/x/tools/cmd/gonew@latest
     gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
