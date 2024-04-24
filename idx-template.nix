1738ad8c7e2d88ca14a50075cbd2eabc41a1226a
Suzy Mueller <suzmue@golang.org>
2024-04-24 13:20:18 -0700

    access bin

--- idx-template.nix
+++ idx-template.nix
@@ -11,9 +11,10 @@
     mkdir "go"
     chmod -R +w "go"
     echo "Go Install:"
-    GOMODCACHE=$(mktemp -d) GOPATH=$(mktemp -d) GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
+    GOPATH=$(mktemp -d)
+    GOMODCACHE=$(mktemp -d) GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
     echo "gonew:"
-    GOMODCACHE=$(mktemp -d) gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
+    $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
