72d12d17298350f86e2a9ebde71669f41d19e9aa
Suzy Mueller <suzmue@golang.org>
2024-04-24 12:21:49 -0700

    set gopath

--- idx-template.nix
+++ idx-template.nix
@@ -1,12 +1,15 @@
 { pkgs, ... }: {
   packages = [ pkgs.go ];
   bootstrap = ''
-    echo "Env:\n"
+    echo "Env:"
     env
-    echo "Go Env:\n"
+    echo "Go Env:"
     go env
-    echo "Go Install:\n"
-    go install golang.org/x/tools/cmd/gonew@latest
+    whoami
+    echo "Mkdir:"
+    mkdir gopath
+    echo "Go Install:"
+    GOPATH='/gopath' go install golang.org/x/tools/cmd/gonew@latest
     echo "gonew:\n"
     gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
