85e02a678ab19b4bd0f6fa9916ad57d5a9a9a900
Suzy Mueller <suzmue@golang.org>
2024-04-24 14:11:32 -0700

    remove extra envvars

--- idx-template.nix
+++ idx-template.nix
@@ -2,7 +2,7 @@
   packages = [ pkgs.go ];
   bootstrap = ''
     export GOPATH=$(mktemp -d)
-    GOMODCACHE=$(mktemp -d) GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
+    go install golang.org/x/tools/cmd/gonew@latest
     $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini example.com/gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
