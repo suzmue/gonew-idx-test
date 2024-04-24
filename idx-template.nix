f01919c9de11e20685815e6685706685eb74b585
Suzy Mueller <suzmue@golang.org>
2024-04-24 13:30:49 -0700

    delete unnecessary

--- idx-template.nix
+++ idx-template.nix
@@ -1,19 +1,8 @@
 { pkgs, ... }: {
   packages = [ pkgs.go ];
   bootstrap = ''
-    echo "env:"
-    env
-    echo "Go Env:"
-    go env
-
-    echo "mkdir:"
-    pwd
-    mkdir "go"
-    chmod -R +w "go"
-    echo "Go Install:"
     export GOPATH=$(mktemp -d)
     GOMODCACHE=$(mktemp -d) GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
-    echo "gonew:"
     $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini example.com/gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
