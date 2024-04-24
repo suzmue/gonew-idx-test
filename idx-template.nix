a93e14c447b4518d69b5d7074d1df5699f4f90d0
Suzy Mueller <suzmue@golang.org>
2024-04-24 13:27:58 -0700

    fix outpath

--- idx-template.nix
+++ idx-template.nix
@@ -14,7 +14,7 @@
     export GOPATH=$(mktemp -d)
     GOMODCACHE=$(mktemp -d) GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
     echo "gonew:"
-    $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
+    $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini example.com/gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
