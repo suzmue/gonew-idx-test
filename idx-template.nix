381597fc63a585373d4b90241925b2f020fcac7a
Suzy Mueller <suzmue@golang.org>
2024-04-24 14:35:32 -0700

    add gocache back

--- idx-template.nix
+++ idx-template.nix
@@ -2,7 +2,7 @@
   packages = [ pkgs.go ];
   bootstrap = ''
     export GOPATH=$(mktemp -d)
-    go install golang.org/x/tools/cmd/gonew@latest
+    GOCACHE=$(mktemp -d) go install golang.org/x/tools/cmd/gonew@latest
     $GOPATH/bin/gonew github.com/suzmue/gemini-template/go-gemini example.com/gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
