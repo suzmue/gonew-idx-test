8cac9a995b748cc4701cdfd198176be6782e84fd
Suzy Mueller <suzmue@golang.org>
2024-04-24 12:16:53 -0700

    annotate

--- idx-template.nix
+++ idx-template.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }: {
   packages = [ pkgs.go ];
   bootstrap = ''
+    echo "Env:\n"
     env
+    echo "Go Env:\n"
     go env
-    go env -w GOMODCACHE="$PWD/go/pkg/mod"
+    echo "Go Install:\n"
     go install golang.org/x/tools/cmd/gonew@latest
+    echo "gonew:\n"
     gonew github.com/suzmue/gemini-template/go-gemini gemini-template "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
